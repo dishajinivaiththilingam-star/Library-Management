fc7d87d2c3a2e728021ed913cd816711751ea507
kobithan2811 <kobikobithan2811@gmail.com>
2026-03-25 10:08:17 +0530

    created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import AddordeleteBook from "./Component/AddordeleteBook/AddordeleteBook";
-import Booklist from "./Component/BookList/Booklist"
-import Navbar from "./Component/NavBar/Navbar"
+
+import Booklist from "./Component/BookList/Booklist";
+import Navbar from "./Component/NavBar/Navbar";
+
 
 
 function App() {
   return (
     <>
-      <AddordeleteBook/>
-       <Navbar/>
-       <Booklist/>
+    <Navbar/>
+      <Booklist/>
     </>
   );
 }
