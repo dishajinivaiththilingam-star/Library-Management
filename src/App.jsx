acba7b3140a13f5bea8a84ac556517e57ce7d217
dishajinivaiththilingam-star <dishajinivaiththilingam@gmail.com>
2026-03-26 09:52:32 +0530

    created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import AddordeleteBook from "./Component/AddordeleteBook/AddordeleteBook"
 import Booklist from "./Component/BookList/Booklist"
-import Donate from "./Component/Donate/Donate"
-import Home from "./Component/Home/Home"
+// import Donate from "./Component/Donate/Donate"
+// import Home from "./Component/Home/Home"
 import Homelist from "./Component/Home/Homelist"
 import Navbar from "./Component/NavBar/Navbar"
 import Donate from "./Component/Donate/Donate"
@@ -14,11 +14,11 @@ function App() {
   return (
     <>
        <Navbar/>
-       <Home/>
+       {/* <Home/> */}
        <Homelist/>
        <Booklist/>
        <AddordeleteBook/>
-       <Donate/>
+       {/* <Donate/> */}
        
     <Home/>
      <Donate/>
