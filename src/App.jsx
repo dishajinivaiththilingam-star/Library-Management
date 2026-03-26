92b2e0382c43f4b1247fa0be635bf2d2c425c4e5
dishajinivaiththilingam-star <dishajinivaiththilingam@gmail.com>
2026-03-26 11:07:04 +0530

    create

--- src/App.jsx
+++ src/App.jsx
@@ -14,14 +14,16 @@ function App() {
   return (
     <>
        <Navbar/>
-       {/* <Home/> */}
+        <Home/>
        <Homelist/>
        <Booklist/>
+       <Donate/>
        <AddordeleteBook/>
        {/* <Donate/> */}
+        {/* <Home/> */}
        
-    <Home/>
-     <Donate/>
+  
+     
     </>
   )
 }
